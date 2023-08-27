3441a06f732f45c8f3fe672191cdf7d97e996081
Luandro <luandro@digital-democracy.org>
2023-08-27 13:16:27 -0300

    Added routes for icons, path, presets, and root.

---
+++ src/routes/icons.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+
+router.get('/:iconName', (req, res) => {
+  const iconName = req.params.iconName;
+  const iconsDir = path.join(mapeoConfigFolder, "icons");
+  const iconPath = path.join(iconsDir, iconName);
+
+  if (fs.existsSync(iconPath) && path.extname(iconPath) === ".svg") {
+    const svgData = fs.readFileSync(iconPath, "utf-8");
+    res.header("Content-Type", "image/svg+xml");
+    res.send(svgData);
+  } else {
+    res.status(404).json({ error: "Icon not found." });
+  }
+});
+
+module.exports = router;
---
+++ src/routes/presets.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+
+router.get('/', (req, res) => {
+  const reqHostname = req.hostname;
+  const protocol = req.protocol;
+  if (fs.existsSync(presetsDir) && fs.readdirSync(presetsDir).length > 0) {
+    fs.readdir(presetsDir, (err, files) => {
+      if (err) {
+        res.status(500).json({ error: "Failed to read presets directory." });
+        return;
+      }
+      const presets = files.map((file) => {
+        const filePath = path.join(presetsDir, file);
+        if (fs.lstatSync(filePath).isDirectory()) {
+          return null;
+        }
+        const rawData = fs.readFileSync(filePath, "utf-8");
+        return JSON.parse(rawData);
+      });
+      res.json(
+        presets
+          .filter((i) => i)
+          .sort(presetCompare)
+          .map((i) => {
+            const { icon } = i;
+            return {
+              ...i,
+              iconPath: `${protocol}://${reqHostname}:${port}/icons/${icon}-100px.svg`,
+            };
+          }),
+      );
+    });
+  } else {
+    res.status(500).json({ error: "No presets found." });
+  }
+});
+
+module.exports = router;
