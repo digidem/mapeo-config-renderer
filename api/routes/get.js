9901e4f0482ef9e05977123a8b630027f09de38a
Tomás Ciccola <tciccola@digital-democracy.com>
2026-01-29 12:26:59 -0300

    added fallback route for each GET endpoint to serve default in the api

--- api/routes/get.js
+++ api/routes/get.js
@@ -5,12 +5,43 @@ const envPort = process.env.PORT || 5000;
   @param {Express} app
 */
 function get(app) {
-  app.get("/api/catfile/:catfile/presets/", async (req, res) => {
-    try {
-      const { hostname, protocol } = req;
-      const catfile = await loadCatfile(req.params.catfile);
-      log("Getting presets");
-      for (let [key, category] of catfile.categories) {
+  app.get(
+    ["/api/catfile/:catfile/presets/", "/api/presets"],
+    async (req, res) => {
+      try {
+        const { hostname, protocol } = req;
+        const catfile = await loadCatfile(req.params.catfile || "default");
+        log("Getting presets");
+        for (let [key, category] of catfile.categories) {
+          category.iconPath = normalizeIconPath(
+            category.icon,
+            req.params.catfile,
+            protocol,
+            hostname,
+            envPort,
+          );
+          catfile.categories.set(key, category);
+        }
+        res.json(Object.fromEntries(catfile.categories));
+        log(`Served presets: ${catfile.categories.size} items`);
+      } catch (error) {
+        res
+          .status(500)
+          .json({ error: error || "Unknown error on getting presets" });
+        log("Error serving presets", error);
+      }
+    },
+  );
+
+  app.get(
+    ["/api/preset/:presetName", "/api/catfile/:catfile/preset/:presetName"],
+    async (req, res) => {
+      try {
+        const presetName = req.params.presetName;
+        const protocol = req.protocol;
+        const hostname = req.hostname;
+        const catfile = await loadCatfile(req.params.catfile || "default");
+        const category = catfile.categories.get(presetName);
         category.iconPath = normalizeIconPath(
           category.icon,
           req.params.catfile,
@@ -18,46 +49,21 @@ function get(app) {
           hostname,
           envPort,
         );
-        catfile.categories.set(key, category);
+        res.json(category);
+      } catch (error) {
+        res.status(500).json({
+          error: "Failed to get preset " + req.params.presetName,
+          message: error.message,
+        });
+        log("Error serving fields", error);
       }
-      res.json(Object.fromEntries(catfile.categories));
-      log(`Served presets: ${catfile.categories.size} items`);
-    } catch (error) {
-      res
-        .status(500)
-        .json({ error: error || "Unknown error on getting presets" });
-      log("Error serving presets", error);
-    }
-  });
+    },
+  );
 
-  app.get("/api/catfile/:catfile/preset/:presetName", async (req, res) => {
-    try {
-      const presetName = req.params.presetName;
-      const protocol = req.protocol;
-      const hostname = req.hostname;
-      const catfile = await loadCatfile(req.params.catfile);
-      const category = catfile.categories.get(presetName);
-      category.iconPath = normalizeIconPath(
-        category.icon,
-        req.params.catfile,
-        protocol,
-        hostname,
-        envPort,
-      );
-      res.json(category);
-    } catch (error) {
-      res.status(500).json({
-        error: "Failed to get preset " + req.params.presetName,
-        message: error.message,
-      });
-      log("Error serving fields", error);
-    }
-  });
-
-  app.get("/api/catfile/:catfile/fields", async (req, res) => {
+  app.get(["/api/fields", "/api/catfile/:catfile/fields"], async (req, res) => {
     try {
       log("Getting fields");
-      const catfile = await loadCatfile(req.params.catfile);
+      const catfile = await loadCatfile(req.params.catfile || "default");
       const data = catfile.fields;
       log("Got fields", data.size);
       res.json(Object.fromEntries(data));
@@ -70,48 +76,58 @@ function get(app) {
     }
   });
 
-  app.get("/icons/catfile/:catfile/:iconName", async (req, res) => {
-    const iconName = req.params.iconName;
+  app.get(
+    ["/icons/:iconName", "/icons/catfile/:catfile/:iconName"],
+    async (req, res) => {
+      const iconName = req.params.iconName;
 
-    try {
-      const catfile = await loadCatfile(req.params.catfile);
-      let data = await catfile.reader.getIcon(iconName);
+      try {
+        const catfile = await loadCatfile(req.params.catfile || "default");
+        let data = await catfile.reader.getIcon(iconName);
 
-      if (!data) {
-        res.status(404).json({ error: "Icon not found." });
-        log(`Failed to serve icon: ${iconName}`);
-      } else {
-        res.header("Content-Type", "image/svg+xml");
-        res.send(data);
-        log(`Served icon: ${iconName}`);
+        if (!data) {
+          res.status(404).json({ error: "Icon not found." });
+          log(`Failed to serve icon: ${iconName}`);
+        } else {
+          res.header("Content-Type", "image/svg+xml");
+          res.send(data);
+          log(`Served icon: ${iconName}`);
+        }
+      } catch (err) {
+        res.status(404).json({ error: "Icon not found.", message: err });
+        log(`Failed to serve icon: ${iconName}`, err);
       }
-    } catch (err) {
-      res.status(404).json({ error: "Icon not found.", message: err });
-      log(`Failed to serve icon: ${iconName}`, err);
-    }
-  });
+    },
+  );
 
-  app.get("/api/catfile/:catfile/metadata", async (req, res) => {
-    try {
-      log("Getting metadata");
-      const catfile = await loadCatfile(req.params.catfile);
+  app.get(
+    ["/api/metadata", "/api/catfile/:catfile/metadata"],
+    async (req, res) => {
+      try {
+        log("Getting metadata");
+        const catfile = await loadCatfile(req.params.catfile || "default");
 
-      const data = catfile.metadata;
-      log("Got metadata", data);
-      res.json(data);
-      log(`Served metadata`);
-    } catch (error) {
-      res
-        .status(500)
-        .json({ error: "Failed to get metadata", message: error.message });
-      log("Error serving metadata", error);
-    }
-  });
-  app.get("/api/catfile/:catfile/categorySelection", async (req, res) => {
-    const catfile = await loadCatfile(req.params.catfile);
+        const data = catfile.metadata;
+        log("Got metadata", data);
+        res.json(data);
+        log(`Served metadata`);
+      } catch (error) {
+        res
+          .status(500)
+          .json({ error: "Failed to get metadata", message: error.message });
+        log("Error serving metadata", error);
+      }
+    },
+  );
 
-    res.json(catfile.categorySelection);
-  });
+  app.get(
+    ["/api/categorySelectoin", "/api/catfile/:catfile/categorySelection"],
+    async (req, res) => {
+      const catfile = await loadCatfile(req.params.catfile || "default");
+
+      res.json(catfile.categorySelection);
+    },
+  );
   app.get("/api/messages", async (req, res) => {
     // try {
     //   log("Getting messages");
